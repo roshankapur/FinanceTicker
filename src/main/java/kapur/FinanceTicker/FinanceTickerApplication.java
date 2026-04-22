068350d18ef4f71e6eb40909e9eed83ee4db76f9
Roshan Kapur <kapurroshan2003@gmail.com>
2026-04-22 23:00:41 +0530

    setup scaffolding for spring project

---
+++ src/main/java/kapur/FinanceTicker/FinanceTickerApplication.java
@@ -0,0 +1,15 @@
+package kapur.FinanceTicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTickerApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World! New Project");
+		SpringApplication.run(FinanceTickerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kapur/FinanceTicker/FinanceTickerApplicationTests.java
@@ -0,0 +1,13 @@
+package kapur.FinanceTicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
